727c936b7f734f8c904a7da87008f26c6b83e3c1
miamiworldwide <rivendalejones@gmail.com>
2017-01-26 20:56:55 -0800

    first commit

---
+++ bands.js
@@ -0,0 +1,30 @@
+var band = function(name){
+  //Automatically created by interpreter
+  Object.keys(bands).forEach(function(name){
+    var band = favBands[name];
+    console.log(band);
+  })
+
+};
+
+
+var firstNames = {
+  "Christian"//,
+/*  "Alex",
+  "Charlie",
+  "Joshua",
+  "Alden",
+  "Sea",
+  "Ripley",
+  "Tamara",
+  "Lauren",
+  "Lark",
+  "Collin",
+  "Stefano" */
+};
+
+var favBands = {
+  "stereolab"
+};
+
+band("Christian");
---
+++ carExample.js
@@ -0,0 +1,13 @@
+var Car = function(color){
+  //Automatically created by interpreter
+  //var this = Object.create(Car.prototype);
+  this.color = color;
+  this.door = 'open';
+
+  //return this;
+};
+
+var car = new Car('red');
+
+
+console.log(car);
